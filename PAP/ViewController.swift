b8a75cc11af9a628bef331642e711b06eeae2c36
Tadeas Bronis <tadeas.bronis@gmail.com>
2016-03-10 08:34:32 +0100

    Initial Commit

---
+++ PAP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PAP
+//
+//  Created by Tadeas Bronis on 10/03/16.
+//  Copyright © 2016 Durech & Bronis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
