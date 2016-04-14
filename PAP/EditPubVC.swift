36781215949d6c178de68aac5c8cc31c0c5bfaae
Matadot <tadeas.bronis@gmail.com>
2016-04-14 09:24:00 +0200

    pridavanie hodnot do editu

--- PAP/EditPubVC.swift
+++ PAP/EditPubVC.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class EditPubVC: UIViewController {
+    
+    @IBOutlet weak var imageField: UITextField!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var addressField: UITextField!
+    @IBOutlet weak var phoneField: UITextField!
+    @IBOutlet weak var hoursField: UITextField!
+    @IBOutlet weak var smokingSwitch: UISwitch!
+    @IBOutlet weak var pricesSlider: UISlider!
+    @IBOutlet weak var staffSlider: UISlider!
+    @IBOutlet weak var atmosphereSlider: UISlider!
+    @IBOutlet weak var chicksSlider: UISlider!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        imageField.text=pubs[actual!]._photo
+        nameField.text=pubs[actual!]._name
+        addressField.text=pubs[actual!]._address
+        phoneField.text=pubs[actual!]._phone
+        hoursField.text=pubs[actual!]._openingHours
+        smokingSwitch.setOn(pubs[actual!]._smoking, animated:true)
+        pricesSlider.setValue(Float(pubs[actual!]._ratingPrices!)/5, animated: true)
+        staffSlider.setValue(Float(pubs[actual!]._ratingStaff!)/5, animated: true)
+        atmosphereSlider.setValue(Float(pubs[actual!]._ratingAtmosphere!)/5, animated: true)
+        chicksSlider.setValue(Float(pubs[actual!]._ratingChicks!)/5, animated: true)
+        
         // Do any additional setup after loading the view.
     }
 
